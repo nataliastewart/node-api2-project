e041a67ffb05a1dc6834a9d6da371c408800e228
Natalia <nataliamirandac@hotmail.com>
2020-06-04 08:32:02 -0400

    GET request to /api/posts/:id - Working - Postman

--- data/routers/posts-router.js
+++ data/routers/posts-router.js
@@ -18,4 +18,88 @@ router.get("/", (req, res) => {
     });
 });
 
+////////
+
+// endpoint for /api/posts/:id
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "post not found" });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the post",
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  Posts.add(req.body)
+    .then((hub) => {
+      res.status(201).json(hub);
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the hub",
+      });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Posts.remove(req.params.id)
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ message: "The hub has been nuked" });
+      } else {
+        res.status(404).json({ message: "The hub could not be found" });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error removing the hub",
+      });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  Posts.update(req.params.id, changes)
+    .then((hub) => {
+      if (hub) {
+        res.status(200).json(hub);
+      } else {
+        res.status(404).json({ message: "The hub could not be found" });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating the hub",
+      });
+    });
+});
+
+router.get("/:id/messages", function (req, res) {
+  Posts.findHubMessages(req.params.id)
+    .then((messages) => {
+      res.status(200).json(messages);
+    })
+    .catch((error) => {
+      //log to a data base or a monitoring system
+
+      res.status(500).json({ message: "error getting messages" });
+    });
+});
+
 module.exports = router;
